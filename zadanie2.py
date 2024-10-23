e8389e953cc328517fbae7c65d5c99ef6694801a
KaroSigma <Karola9873@gmail.com>
2024-10-23 20:30:14 +0200

    Dodanie komentarza z trescia zadania. Zapomnialam wczesniej dodac

--- zadanie2.py
+++ zadanie2.py
@@ -1,3 +1,18 @@
+"""W sklepie ze sprzętem AGD oferowana jest sprzedaż ratalna. Napisz program
+umożliwiający wyliczenie wysokości miesięcznej raty za zakupiony sprzęt. Danymi
+wejściowymi dla programu są:
+� cena towaru (od 100 zł do 10 tyś. zł),
+� liczba rat (od 6 do 48).
+Kredyt jest oprocentowany w zależności od liczby rat:
+� od 6–12 wynosi 2.5% ,
+� od 13–24 wynosi 5%,
+� od 25–48 wynosi 10%.
+Obliczona miesięczna rata powinna zawierać również odsetki. Program powinien
+sprawdzać, czy podane dane mieszczą się w określonych powyżej zakresach, a w
+przypadku błędu prosić użytkownika ponownie o podanie danych. Sposób liczenia
+odsetek może być najprostszy (od aktualnej kwoty zadłużenia).
+"""
+
 #dopoki nie bedzie break to nie przestanie pytac o cene
 while True:
   cena = float(input("Podaj cene towaru w przedziale 100 do 10000 zł: "))
